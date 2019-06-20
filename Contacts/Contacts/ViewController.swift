4d7bbe9c8cadf8364f3c3ab0fc41f48d90e37dbf
Kalangutkar Ankita Vinod <ankita.kalangutkar@photoninfotech.net>
2019-06-20 16:21:24 +0530

    Initial Project setup

---
+++ Contacts/Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Kalangutkar Ankita Vinod on 6/20/19.
+//  Copyright © 2019 Kalangutkar Ankita Vinod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
